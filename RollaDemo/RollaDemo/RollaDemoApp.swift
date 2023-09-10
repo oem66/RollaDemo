e1065298a7b3935a01396cd868f61ceccfbbe9ad
Omer Rahmanovic <omer.6g@gmail.com>
2023-09-10 10:44:33 +0200

    add project files

---
+++ RollaDemo/RollaDemo/RollaDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  RollaDemoApp.swift
+//  RollaDemo
+//
+//  Created by Omer Rahmanovic on 10. 9. 2023..
+//
+
+import SwiftUI
+
+@main
+struct RollaDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
